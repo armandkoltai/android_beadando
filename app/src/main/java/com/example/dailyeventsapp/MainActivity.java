fd0f30e21aeeeabab7de21b325e8c382dae96e7e
Hoki Attila <hoki.attila@gmail.com>
2025-04-26 22:37:25 +0200

    Added Toolbar and BottomNavigation to Mainactivity alongside with fragmenthandling

--- app/src/main/java/com/example/dailyeventsapp/MainActivity.java
+++ app/src/main/java/com/example/dailyeventsapp/MainActivity.java
@@ -4,21 +4,50 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.dailyeventsapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+   Toolbar toolbar;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.homemenu) {
+                replaceFragment(new HomeFragment());
+            } else if (id == R.id.savedmenu) {
+                replaceFragment(new SavedFragment());
+            } else if (id == R.id.infomenu) {
+                replaceFragment(new InfoFragment());
+            }
+            return true;
         });
+
+        this.toolbar = findViewById(R.id.toolbar);
+        toolbar.setContentInsetStartWithNavigation(0);
+        toolbar.setContentInsetsRelative(0, 16);
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
