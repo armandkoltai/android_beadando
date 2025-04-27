9a0ef5695899ff39b59d60e59fe65d3d1d7202d6
Hoki Attila <hoki.attila@gmail.com>
2025-04-27 02:39:17 +0200

    Added HomeFragment visuals

--- app/src/main/java/com/example/dailyeventsapp/HomeFragment.java
+++ app/src/main/java/com/example/dailyeventsapp/HomeFragment.java
@@ -1,64 +1,116 @@
 package com.example.dailyeventsapp;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Calendar;
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
+    private AppCompatButton submitButton;
 
     public HomeFragment() {
-        // Required empty public constructor
+        
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("param1", param1);
+        args.putString("param2", param2);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        initDatePicker();
+        submitButton = view.findViewById(R.id.submitAppCompatButton);
+        dateButton = view.findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodaysDate());
+
+        dateButton.setOnClickListener(v -> openDatePicker());
+        submitButton.setOnClickListener(v -> {
+            String selectedDate = dateButton.getText().toString();
+            Toast.makeText(requireContext(), "Selected date: " + selectedDate, Toast.LENGTH_SHORT).show();
+        });
+
+
+        return view;
+    }
+
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int month = cal.get(Calendar.MONTH) + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month);
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = (view, year, month, dayOfMonth) -> {
+            month += 1;
+            String date = makeDateString(dayOfMonth, month);
+            dateButton.setText(date);
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(requireContext(), style, dateSetListener, year, month, day);
+
+
+        DatePicker datePicker = datePickerDialog.getDatePicker();
+        int yearId = getResources().getIdentifier("year", "id", "android");
+        View yearView = datePicker.findViewById(yearId);
+        if (yearView != null) {
+            yearView.setVisibility(View.GONE);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    private String makeDateString(int day, int month) {
+        return getMonthFormat(month) + " " + String.format("%02d", day);
+    }
+
+    private String getMonthFormat(int month) {
+        switch (month) {
+            case 1: return "JAN";
+            case 2: return "FEB";
+            case 3: return "MAR";
+            case 4: return "APR";
+            case 5: return "MAY";
+            case 6: return "JUN";
+            case 7: return "JUL";
+            case 8: return "AUG";
+            case 9: return "SEP";
+            case 10: return "OCT";
+            case 11: return "NOV";
+            case 12: return "DEC";
+            default: return "JAN";
+        }
+    }
+
+    private void openDatePicker() {
+        datePickerDialog.show();
     }
 }
